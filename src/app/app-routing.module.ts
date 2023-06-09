14759301fcc7f3eacce790fbad028a4cdc98e001
MycolaAnikeiev <mykolaanikeiev@gmail.com>
2023-06-09 12:57:26 +0300

    feat: make app modules and routes sceleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    title: "Online Cource Landing",
+    path: '',
+    pathMatch: "full",
+    loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)
+  },
+  { 
+    title: "Online Cource Landing",
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule) 
+  },
+  { 
+    title: "Login",
+    path: 'login', 
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { 
+    title: "Registration",
+    path: 'registration', 
+    loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule) 
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
