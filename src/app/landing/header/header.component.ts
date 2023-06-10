853cc38042aa992a085677ac442040e853dd95e9
MycolaAnikeiev <mykolaanikeiev@gmail.com>
2023-06-10 15:46:33 +0300

    feat: add lending page header

--- src/app/landing/header/header.component.ts
+++ src/app/landing/header/header.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: [
+    '../common.css',
+    './header.component.css',
+    './header.component.menu.css'
+  ]
 })
 export class HeaderComponent {
+  menuOpen = false
 
+  onMenuClick(){
+    this.menuOpen = !this.menuOpen
+  }
 }
