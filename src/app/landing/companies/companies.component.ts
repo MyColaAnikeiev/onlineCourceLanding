54455dce464e70796aee80104330b0d7079850e8
MycolaAnikeiev <mykolaanikeiev@gmail.com>
2023-06-12 22:59:50 +0300

    feat: add layout for commpanies section

--- src/app/landing/companies/companies.component.ts
+++ src/app/landing/companies/companies.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
-  styleUrls: ['./companies.component.css']
+  styleUrls: [
+    '../common.css',
+    './companies.component.css'
+  ]
 })
 export class CompaniesComponent {
 
